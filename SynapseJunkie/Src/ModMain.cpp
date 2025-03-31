9c518d2e59ac9e24f6be5f03f50f8b9fdb059ae9
thelivingdiamond <thelivingdiamond6@gmail.com>
2025-03-31 14:54:08 -0700

    Cleanup example code

--- SynapseJunkie/Src/ModMain.cpp
+++ SynapseJunkie/Src/ModMain.cpp
@@ -9,53 +9,6 @@
 
 ModMain* gMod = nullptr;
 
-#ifdef EXAMPLE
-// You can define PreyDll.dll function using PreyFunction
-// auto fnFuncName = PreyFunction<void(T funcArgs)>(functionOffset);
-// Many functions are defined as static members with F prefix:
-//  ArkPlayer.h
-//     static inline auto FHasAbility = PreyFunction<bool(ArkPlayer* _this, uint64_t _abilityID)>(0x1550330);
-static auto fn_ArkPlayer_HasAbility = PreyFunction<bool(ArkPlayer* _this, uint64_t _abilityID)>(0x1550330);
-
-// Use MakeHook() method of PreyFunction to create a hook.
-// See ModMain::InitHooks for more.
-static auto s_hook_ArkPlayer_HasAbility = fn_ArkPlayer_HasAbility.MakeHook();
-
-// This function will be called in place of ArkPlayer::HasAbility.
-// See ModMain::InitHooks for more.
-static bool ArkPlayer_HasAbility_Hook(ArkPlayer* _this, uint64_t _abilityID)
-{
-    // NOTE: This particular method of ability unlocking is unreliable.
-    // NOTE: It's only used as an example of function hooking.
-
-    if (_abilityID % 2 == 0)
-    {
-        // Player always has abilities whose ID is even.
-        return true;
-    }
-
-    // Use InvokeOrig of PreyFunctionHook to call the original (non-hooked) function.
-    return s_hook_ArkPlayer_HasAbility.InvokeOrig(_this, _abilityID);
-}
-
-#endif
-
-#ifdef EXAMPLE
-// Mod Interface Example
-// Define an interface like this in a header file.
-// Then inherit ModMain from that interface.
-struct IModName : public IModInterface
-{
-    static constexpr char IFACE_NAME[] = "IModName001";
-    static constexpr int VERSION = 1;
-
-    virtual int GetInterfaceVersion() = 0;
-
-    //! Does something mod-specific.
-    virtual void ExampleMethod() = 0;
-};
-#endif
-
 
 //---------------------------------------------------------------------------------
 // Mod Initialization
@@ -69,34 +22,14 @@ void ModMain::FillModInfo(ModDllInfoEx& info)
 
 void ModMain::InitHooks()
 {
-#ifdef EXAMPLE
-    // Functions hooks are intalled early into mod loading process,
-    // before any engine subsystem is initialized.
-    // But for hook to succeed PreyFunctionHook needs to know the hook function
-    // (that will be called in place of original one)
-    // That's why ModMain::InitHooks exists.
-    // Call SetHookFunc from here.
-    s_hook_ArkPlayer_HasAbility.SetHookFunc(&ArkPlayer_HasAbility_Hook);
-#endif
+
 }
 
 void ModMain::InitSystem(const ModInitInfo& initInfo, ModDllInfo& dllInfo)
 {
     BaseClass::InitSystem(initInfo, dllInfo);
     // Your code goes here
-    //! CVar example
-#ifdef EXAMPLE
-    // If you want to store variables between restarts, use a CVar with the DUMPTOCHAIR flag.
-    // This will save the value to a file, and the value will be restored on launch.
-    // Cvars can be changed with the console, or by simply changing the variable in code.
-    // A great example would be storing whether a window is open or not.
-    // You will typically use the macros REGISTER_CVAR(...) or REGISTER_CVAR2(...) to register a CVar.
-    // See Common/Prey/CrySystem/IConsole.h for the documentation on CVars
 
-    //! Best used for simple user preferences. For more complex data, You will need to use an actual config file.
-    static int m_bDoSomething = false;
-    REGISTER_CVAR2("ExampleMod.DoSomething", &m_bDoSomething, 0, VF_DUMPTOCHAIR, "ExampleMod.DoSomething");
-#endif
 }
 
 void ModMain::InitGame(bool isHotReloading)
